4816c2948065ccd1d8cae45abb62396ff912e81c
Ubaldo Aviles <ubaldoaviles@Ubaldos-Laptop.local>
2024-02-12 17:45:50 -0600

    App-Shell

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+//Crearmos una constante llamada cacheAppShel que disparará el evento install
+self.addEventListener('install', function(event) {
+    const cacheAppShell = caches.open('cache-v1').then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/js/app.js',
+            "/sw.js",
+            "static/js/bundle.js",
+            "favicon.ico",
+        ]);
+    }
+    );
+    event.waitUntil(cacheAppShell);
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request))
+})
+
